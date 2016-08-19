451e9aa1385b54dafdb4538804a7a921f0918dbe
Tim Poffenbarger <poffey21@gmail.com>
2016-08-19 17:23:27 -0500

    added mockups for framework7

--- crossbar_server/web/f7/js/my-app.js
+++ crossbar_server/web/f7/js/my-app.js
@@ -3,6 +3,9 @@ var myApp = new Framework7();
 
 // Export selectors engine
 var $$ = Dom7;
+ 
+// Fruits data demo array
+var fruits = ('Apple Apricot Avocado Banana Melon Orange Peach Pear Pineapple').split(' ');
 
 // Add view
 var mainView = myApp.addView('.view-main', {
@@ -16,6 +19,32 @@ myApp.onPageInit('about', function (page) {
     $$('.create-page').on('click', function () {
         createContentPage();
     });
+});
+
+
+var autocompleteStandaloneMultiple = myApp.autocomplete({
+    openIn: 'page', //open in page
+    opener: $$('#autocomplete-standalone-multiple'), //link that opens autocomplete
+    multiple: true, //allow multiple values
+    source: function (autocomplete, query, render) {
+        var results = [];
+        if (query.length === 0) {
+            render(results);
+            return;
+        }
+        // Find matched items
+        for (var i = 0; i < fruits.length; i++) {
+            if (fruits[i].toLowerCase().indexOf(query.toLowerCase()) >= 0) results.push(fruits[i]);
+        }
+        // Render items by passing array with result items
+        render(results);
+    },
+    onChange: function (autocomplete, value) {
+        // Add item text value to item-after
+        $$('#autocomplete-standalone-multiple').find('.item-after').text(value.join(', '));
+        // Add item value to input value
+        $$('#autocomplete-standalone-multiple').find('input').val(value.join(', '));
+    }
 });
 
 // Generate dynamic page
